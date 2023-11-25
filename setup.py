f9d4e5a76bfdf62cd3b341e37dcf548889b6e3a8
Anika Arora <anika24.arora@gmail.com>
2023-11-25 00:23:30 -0800

    Initial setup commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = '133afinal'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/rviz',   glob('rviz/*')),
+        ('share/' + package_name + '/urdf',   glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='aarora@caltech.edu',
+    description='ME 133a Final Project: Anika Arora & Andy Dimanku',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            '133amovement1 = 133afinal.p1_bending:main',
+        ],
+    },
+)
