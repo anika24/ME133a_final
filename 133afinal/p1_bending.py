142742402c73bf15aa3025e51f23a17b049ead7e
Anika Arora <anika24.arora@gmail.com>
2023-11-27 15:47:55 -0800

    combined demonode with generator node w/ pelvis broadcast

--- 133afinal/p1_bending.py
+++ 133afinal/p1_bending.py
@@ -20,9 +20,9 @@ from rclpy.time                 import Duration
 from tf2_ros                    import TransformBroadcaster
 from geometry_msgs.msg          import TransformStamped
 from sensor_msgs.msg            import JointState
+from asyncio                    import Future
 
 # Grab the utilities
-from hw5code.GeneratorNode      import GeneratorNode
 from demos.TransformHelpers     import *
 from hw5code.TrajectoryUtils    import *
 
@@ -30,157 +30,11 @@ from hw5code.TrajectoryUtils    import *
 from hw5code.KinematicChain     import KinematicChain
 
 
-jointnames = ['leftHipYaw', 'leftHipRoll', 'leftHipPitch', 
-              'leftKneePitch', 'leftAnklePitch', 'leftAnkleRoll',
-
-              'rightHipYaw', 'rightHipRoll', 'rightHipPitch', 
-              'rightKneePitch', 'rightAnklePitch', 'rightAnkleRoll',
-
-              'torsoYaw', 'torsoPitch', 'torsoRoll',
-              'lowerNeckPitch', 'neckYaw', 'upperNeckPitch',
-
-              'rightShoulderPitch', 'rightShoulderRoll', 'rightShoulderYaw', 
-              'rightElbowPitch', 'rightForearmYaw', 'rightWristRoll', 'rightWristPitch',
-              'rightThumbRoll', 'rightThumbPitch1', 'rightIndexFingerPitch1', 'rightMiddleFingerPitch1',
-              'rightPinkyPitch1',
-
-              'leftShoulderPitch', 'leftShoulderRoll', 'leftShoulderYaw', 
-              'leftElbowPitch', 'leftForearmYaw', 'leftWristRoll', 'leftWristPitch',
-              'leftThumbRoll', 'leftThumbPitch1', 'leftIndexFingerPitch1', 'leftMiddleFingerPitch1',
-              'leftPinkyPitch1'
-            ]
-
-#
-#   Demo Node Class
-#
-class DemoNode(Node):
-    # Initialization.
-    def __init__(self, name, rate):
-        # Initialize the node, naming it as specified
-        super().__init__(name)
-
-        # Initialize the transform broadcaster
-        self.broadcaster = TransformBroadcaster(self)
-
-        # Add a publisher to send the joint commands.
-        self.pub = self.create_publisher(JointState, '/joint_states', 10)
-
-        # Wait for a connection to happen.  This isn't necessary, but
-        # means we don't start until the rest of the system is ready.
-        self.get_logger().info("Waiting for a /joint_states subscriber...")
-        while(not self.count_subscribers('/joint_states')):
-            pass
-
-        # Set up the timing so (t=0) will occur in the first update
-        # cycle (dt) from now.
-        self.dt    = 1.0 / float(rate)
-        self.t     = -self.dt
-        self.start = self.get_clock().now() + Duration(seconds=self.dt)
-
-        # Create a timer to keep calling update().
-        self.create_timer(self.dt, self.update)
-        self.get_logger().info("Running with dt of %f seconds (%fHz)" %
-                               (self.dt, rate))
-
-    # Shutdown.
-    def shutdown(self):
-        # Destroy the node, including cleaning up the timer.
-        self.destroy_node()
-
-    # Return the current time (in ROS format).
-    def now(self):
-        return self.start + Duration(seconds=self.t)
-
-    # Update - send a new joint command every time step.
-    def update(self):
-        # To avoid any time jitter enforce a constant time step and
-        # integrate to get the current time.
-        self.t += self.dt
-
-        # Compute position/orientation of the pelvis (w.r.t. world).
-        ppelvis = pxyz(0.0, 0.5, 0.5 * np.cos(self.t/2))
-        Rpelvis = Rotz(np.sin(self.t))
-        Tpelvis = T_from_Rp(Rpelvis, ppelvis)
-        
-        # Build up and send the Pelvis w.r.t. World Transform!
-        trans = TransformStamped()
-        trans.header.stamp    = self.now().to_msg()
-        trans.header.frame_id = 'world'
-        trans.child_frame_id  = 'pelvis'
-        trans.transform       = Transform_from_T(Tpelvis)
-        self.broadcaster.sendTransform(trans)
-
-        # Compute the joints.
-        q    = np.zeros((len(jointnames), 1))
-        qdot = np.zeros((len(jointnames), 1))
-
-        i_relbow = jointnames.index('rightElbowPitch')
-        i_rshoulder_yaw = jointnames.index('rightShoulderYaw')
-        i_rforearm_yaw = jointnames.index('rightForearmYaw')
-
-        q[i_relbow,0]           = - pi/2 + pi/8 * sin(2*self.t)
-        q[i_rshoulder_yaw,0]    = pi/2
-        q[i_rforearm_yaw,0]     = -pi/2
-        qdot[i_relbow, 0]       = pi/4 * cos(2*self.t)
-
-        # Build up a command message and publish.
-        cmdmsg = JointState()
-        cmdmsg.header.stamp = self.now().to_msg()       # Current time for ROS
-        cmdmsg.name         = jointnames                # List of names
-        cmdmsg.position     = q.flatten().tolist()      # List of positions
-        cmdmsg.velocity     = qdot.flatten().tolist()   # List of velocities
-        self.pub.publish(cmdmsg)
-
-#
-#  Main Code
-#
-def main(args=None):
-    # Initialize ROS and the demo node (100Hz).
-    rclpy.init(args=args)
-    node = DemoNode('p1_bending', 100)
-
-    # Spin, until interrupted.
-    rclpy.spin(node)
-
-    # Shutdown the node and ROS.
-    node.shutdown()
-    rclpy.shutdown()
-
-if __name__ == "__main__":
-    main()
-
-
-class Trajectory():
-    # Initialization.
-    def __init__(self, node):
-        # Set up the kinematic chain object.
-        self.chain_left_leg = KinematicChain(node, 'pelvis', 'leftFoot', self.jointnames('left_leg'))
-        self.chain_right_leg = KinematicChain(node, 'pelvis', 'rightFoot', self.jointnames('right_leg'))
-
-        # Initial q0 is 0 for all joints
-        self.q_pelvis = np.zeros((6, 1))
-        self.q_left_leg = np.zeros((6, 1))
-        self.q_right_leg = np.zeros((6, 1))
-        self.q = np.vstack((self.q_pelvis, self.q_left_leg, self.q_right_leg))
-
-        # Set up initial positions for the chain tips
-        self.pos_left_leg = (np.array([-0.009844, 0.13769, -1.0834]).reshape((-1, 1)), R_from_quat(np.array([0, 0, 0, 1])))
-        self.pos_right_leg = (np.array([-0.0098553, 0.13778, -1.0834]).reshape((-1, 1)), R_from_quat(np.array([0, 0, 0, 1])))
-
-        self.amp = 1
-        self.period = 2*pi
-        self.lam = 20
-
-    # Declare the joint names.
-    def jointnames(self, chain = 'all'):
-        # Return a list of joint names FOR THE EXPECTED URDF!
-
-        # There are different chains stemming from the pelvis, tree structure can be seen in ValkyrieD.pdf
-        chains = {
+joint_names = {
             'left_leg': ['leftHipYaw', 'leftHipRoll', 'leftHipPitch', 'leftKneePitch', 
                          'leftAnklePitch', 'leftAnkleRoll'],
 
-            'right_leg': ['rightHipYaw', 'righHipRoll', 'rightHi,pPitch', 'righKneePitch', 'righAnklePitch',
+            'right_leg': ['rightHipYaw', 'rightHipRoll', 'rightHipPitch', 'rightKneePitch', 'rightAnklePitch',
                         'rightAnkleRoll'],
 
             'left_arm': ['torsoYaw', 'torsoPitch', 'torsoRoll', 'leftShoulderPitch', 'leftShoulderRoll',
@@ -196,58 +50,188 @@ class Trajectory():
             'neck': ['torsoYaw', 'torsoPitch', 'torsoRoll', 'lowerNeckPitch', 'neckYaw', 'upperNeckPitch']
         }
 
-        if chain == 'all':
-            return chains['left_leg'] + chains['right_leg'] + chains['neck'] + chains['left_arm'][3:] + chains['right_arm'][3:]
-        else:
-            return chains[chain]
+#
+#   Trajectory Generator Node Class
+#
+#   This inherits all the standard ROS node stuff, but adds
+#     1) an update() method to be called regularly by an internal timer,
+#     2) a spin() method, aware when a trajectory ends,
+#     3) a shutdown() method to stop the timer.
+#
+#   Take the node name, the update frequency, and the trajectory class
+#   as arguments.
+#
+class GeneratorNode(Node):
+    # Initialization.
+    def __init__(self, name, rate, Trajectory):
+        # Initialize the node, naming it as specified
+        super().__init__(name)
 
-    def pelvis_movement(self, A, w, t):
-        # Define path variable for motion of pelvis to be a cosine wave
-        pos = np.array([0, 0, A - A * cos(w * t)]).reshape((-1,1))
-        R_pos = R_from_quat(np.array([0, 0, 0, 1]))
-        vel = np.array([0, 0, -A * w * sin(w * t)]).reshape((-1,1))
-        return (pos, R_pos), vel
+        # Initialize the transform broadcaster
+        self.broadcaster = TransformBroadcaster(self)
+
+        # Set up a trajectory.
+        self.trajectory = Trajectory(self)
+        self.jointnames = self.trajectory.jointnames()
+
+        # Add a publisher to send the joint commands.
+        self.pub = self.create_publisher(JointState, '/joint_states', 10)
+
+        # Wait for a connection to happen.  This isn't necessary, but
+        # means we don't start until the rest of the system is ready.
+        self.get_logger().info("Waiting for a /joint_states subscriber...")
+        while(not self.count_subscribers('/joint_states')):
+            pass
+
+        # Create a future object to signal when the trajectory ends,
+        # i.e. no longer returns useful data.
+        self.future = Future()
+
+        # Set up the timing so (t=0) will occur in the first update
+        # cycle (dt) from now.
+        self.dt    = 1.0 / float(rate)
+        self.t     = -self.dt
+        self.start = self.get_clock().now()+rclpy.time.Duration(seconds=self.dt)
+
+        # Create a timer to keep calculating/sending commands.
+        self.timer = self.create_timer(self.dt, self.update)
+        self.get_logger().info("Running with dt of %f seconds (%fHz)" %
+                               (self.dt, rate))
+
+    # Shutdown
+    def shutdown(self):
+        # Destroy the timer, then shut down the node.
+        self.timer.destroy()
+        self.destroy_node()
+
+        # Return the current time (in ROS format).
+    def now(self):
+        return self.start + Duration(seconds=self.t)
+
+    # Spin
+    def spin(self):
+        # Keep running (taking care of the timer callbacks and message
+        # passing), until interrupted or the trajectory is complete
+        # (as signaled by the future object).
+        rclpy.spin_until_future_complete(self, self.future)
+
+        # Report the reason for shutting down.
+        if self.future.done():
+            self.get_logger().info("Stopping: " + self.future.result())
+        else:
+            self.get_logger().info("Stopping: Interrupted")
+
+
+    # Update - send a new joint command every time step.
+    def update(self):
+        # To avoid any time jitter enforce a constant time step and
+        # integrate to get the current time.
+        self.t += self.dt
+
+        # Determine the corresponding ROS time (seconds since 1970).
+        now = self.start + rclpy.time.Duration(seconds=self.t)
+
+        # Compute the desired joint positions and velocities for this time.
+        desired = self.trajectory.evaluate(self.t, self.dt)
+        if desired is None:
+            self.future.set_result("Trajectory has ended")
+            return
+        (q, qdot) = desired
+
+        ppelvis = pxyz(0.0, 0, 0.3 * np.cos(self.t/2))
+        Rpelvis = Rotz(np.sin(self.t))
+        Tpelvis = T_from_Rp(Rpelvis, ppelvis)
+        
+        trans = TransformStamped()
+        trans.header.stamp    = self.now().to_msg()
+        trans.header.frame_id = 'world'
+        trans.child_frame_id  = 'pelvis'
+        trans.transform       = Transform_from_T(Tpelvis)
+        self.broadcaster.sendTransform(trans)
+
+        # Check the results.
+        if not (isinstance(q, list) and isinstance(qdot, list)):
+            self.get_logger().warn("(q) and (qdot) must be python lists!")
+            return
+        if not (len(q) == len(self.jointnames)):
+            self.get_logger().warn("(q) must be same length as jointnames!")
+            return
+        if not (len(q) == len(self.jointnames)):
+            self.get_logger().warn("(qdot) must be same length as (q)!")
+            return
+        if not (isinstance(q[0], float) and isinstance(qdot[0], float)):
+            self.get_logger().warn("Flatten NumPy arrays before making lists!")
+            return
+
+        # Build up a command message and publish.
+        cmdmsg = JointState()
+        cmdmsg.header.stamp = now.to_msg()      # Current time for ROS
+        cmdmsg.name         = self.jointnames   # List of joint names
+        cmdmsg.position     = q                 # List of joint positions
+        cmdmsg.velocity     = qdot              # List of joint velocities
+        self.pub.publish(cmdmsg)
+
+class Trajectory():
+    # Initialization.
+    def __init__(self, node):
+        #   Set up the kinematic chain object.
+        self.chain_left_leg = KinematicChain(node, 'pelvis', 'leftFoot', joint_names['left_leg'])
+        self.chain_right_leg = KinematicChain(node, 'pelvis', 'rightFoot', joint_names['right_leg'])
+
+        # Initial q0 is 0
+        self.q_left_leg = np.zeros((6, 1))
+        self.q_right_leg = np.zeros((6, 1))
+        self.q = np.vstack((self.q_left_leg, self.q_right_leg))
+
+        # Set up initial positions for the chain tips, with respect to the pelvis
+        self.pos_left_leg = (np.array([-0.010126, -0.1377, -1.0834]).reshape((-1, 1)), R_from_quat(np.array([0, 0, 0, 1])))
+        self.pos_right_leg = (np.array([-0.010126, -0.1377, -1.0834]).reshape((-1, 1)), R_from_quat(np.array([0, 0, 0, 1])))
+
+        self.amp = 0.5
+        self.period = 0.5
+        self.lam = 20
+
+    # Declare the joint names.
+    def jointnames(self):
+        # Return a list of joint names FOR THE EXPECTED URDF!
+        return joint_names['left_leg'] + joint_names['right_leg'] + joint_names['neck'] + joint_names['left_arm'][3:] + joint_names['right_arm'][3:]
 
     # Evaluate at the given time.  This was last called (dt) ago.
     def evaluate(self, t, dt):
-        # desired position/velocities for pelvis only, everything else is stationary
-        pd, vd = self.pelvis_movement(self.amp, self.period, t)
-        Rd = R_from_quat(np.array([0, 0, 0, 1]))
+        # Compute the joints.
+        # left_leg_qlast = self.q_left_leg
+        # right_leg_qlast = self.q_right_leg
 
-        pelvis_qlast = self.q_pelvis
-        left_leg_qlast = self.q_left_leg
-        right_leg_qlast = self.q_right_leg
+        # qlast = self.q
+        # (p_ll, R_ll, Jv_ll, Jw_ll) = self.chain_world_pelvis.fkin(left_leg_qlast)
+        # (p_rl, R_rl, Jv_rl, Jw_rl) = self.chain_world_pelvis.fkin(right_leg_qlast)
+        # J_ll = np.stack((Jv_ll, Jw_ll))
+        # J_rl = np.stack((Jv_rl, Jw_rl))
 
-        qlast = self.q
-        (p_p, R_p, Jv_p, Jw_p) = self.chain_world_pelvis.fkin(pelvis_qlast)
-        (p_ll, R_ll, Jv_ll, Jw_ll) = self.chain_world_pelvis.fkin(left_leg_qlast)
-        (p_rl, R_rl, Jv_rl, Jw_rl) = self.chain_world_pelvis.fkin(right_leg_qlast)
-        J_p = np.stack((Jv_p, Jw_p))
-        J_ll = np.stack((Jv_ll, Jw_ll))
-        J_rl = np.stack((Jv_rl, Jw_rl))
+        # ep_p, er_p = ep(pd, p_p), eR(Rd, R_p)
+        # ep_ll, er_ll = ep(self.pos_left_leg[0], p_ll), eR(self.pos_left_leg[1], R_ll)
+        # ep_rl, er_rl = ep(self.pos_right_leg[0], p_rl), eR(self.pos_right_leg[1], R_rl)
 
-        ep_p, er_p = ep(pd, p_p), eR(Rd, R_p)
-        ep_ll, er_ll = ep(self.pos_left_leg[0], p_ll), eR(self.pos_left_leg[1], R_ll)
-        ep_rl, er_rl = ep(self.pos_right_leg[0], p_rl), eR(self.pos_right_leg[1], R_rl)
+        # e = np.vstack((ep_p, er_p, ep_ll, er_ll, ep_rl, er_rl))
 
-        e = np.vstack((ep_p, er_p, ep_ll, er_ll, ep_rl, er_rl))
+        # J = np.block([[J_p, np.zeros_like(J_p), np.zeros_like(J_p)],
+        #                 [np.zeros_like(J_ll), J_ll, np.zeros_like(J_ll)],
+        #                 [np.zeros_like(J_rl), np.zeros_like(J_rl), J_rl]])
 
-        J = np.block([[J_p, np.zeros_like(J_p), np.zeros_like(J_p)],
-                      [np.zeros_like(J_ll), J_ll, np.zeros_like(J_ll)],
-                      [np.zeros_like(J_rl), np.zeros_like(J_rl), J_rl]])
+        # v = np.zeros((18, 1))
+        # v[0:3] = vd
 
-        v = np.zeros((18, 1))
-        v[0:3] = vd
+        # qdot = np.linalg.pinv(J) @ (v + self.lam * e)
+        # q = qlast + dt * qdot
+        # self.q_pelvis = q[:6]
+        # self.q_left_leg = q[6:12]
+        # self.q_right_leg = q[12:]
+        # self.q = q
 
-        qdot = np.linalg.pinv(J) @ (v + self.lam * e)
-        q = qlast + dt * qdot
-        self.q_pelvis = q[:6]
-        self.q_left_leg = q[6:12]
-        self.q_right_leg = q[12:]
-        self.q = q
+        q    = np.zeros((len(self.jointnames()), 1))
+        qdot = np.zeros((len(self.jointnames()), 1))
 
         return (q.flatten().tolist(), qdot.flatten().tolist())
-
 
 #
 #  Main Code
